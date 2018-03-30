f1368b3ff48b2d784ada5edcd69e8c93ce4545e0
Hosam <hossmnasser938@gmail.com>
2018-03-30 14:17:49 +0200

    Add PodcastActivity

---
+++ app/src/main/java/com/example/android/ajhogepodcasts/PodcastsActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.ajhogepodcasts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PodcastsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_podcasts);
+    }
+}
